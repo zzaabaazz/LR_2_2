d5929fdb1c60dc929dc222adf626d62e34a2cbe8
stud034163 <stud034163@HOMENET75>
2023-10-18 10:32:42 +0300

    Добавьте файлы проекта.

---
+++ LR_2_2/Catalog.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+namespace XML
+{
+    /// <summary>
+    /// Каталог телефонных номеров.
+    /// </summary>
+    public class Catalog
+    {
+        /// <summary>
+        /// Список телефонных номеров.
+        /// </summary>
+        public List<Phone> Phones { get; set; } = new List<Phone>();
+    }
+}
---
+++ LR_2_2/Phone.cs
@@ -0,0 +1,33 @@
+﻿namespace XML
+{
+    /// <summary>
+    /// Телефонный контакт.
+    /// </summary>
+    public class Phone
+    {
+        /// <summary>
+        /// Имя.
+        /// </summary>
+        public string Model { get; set; }
+        /// <summary>
+        /// Модель.
+        /// </summary>
+        public string Brand { get; set; }
+        /// <summary>
+        /// фирма-изготовитель
+        /// </summary>
+        public string Specs { get; set; }
+        /// <summary>
+        /// характеристики
+        /// </summary>
+        public double Price { get; set; }
+        /// <summary>
+        /// стоимость
+        /// </summary>
+        /// <returns> Имя. </returns>
+        public override string ToString()
+        {
+            return Model;
+        }
+    }
+}
